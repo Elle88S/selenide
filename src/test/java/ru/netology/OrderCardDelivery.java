8cb48983b1588c807061548539127b210986b043
Elle <elle.sokolova92@gmail.com>
2024-03-27 16:35:35 +0300

    add date at last test

--- src/test/java/ru/netology/OrderCardDelivery.java
+++ src/test/java/ru/netology/OrderCardDelivery.java
@@ -19,39 +19,36 @@ import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
 public class OrderCardDelivery {
 
-        public String actualDate () {
+    public String actualDate() {
 
-            LocalDate date = LocalDate.now();
-            date = date.plusDays(5);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            String text = date.format(formatter);
-            LocalDate parsedDate = LocalDate.parse(text, formatter);
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(5);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String text = date.format(formatter);
+        LocalDate parsedDate = LocalDate.parse(text, formatter);
 
-            return text;
-        }
+        return text;
+    }
 
 
     @Test
-    void deliveryCardForm(){
+    void deliveryCardForm() {
 
 
         open("http://localhost:9999/");
         SelenideElement form = $("[id=root]");
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE);
+        form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         form.$("[data-test-id=date] input").setValue(actualDate());
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
         form.$("[name='phone']").setValue("+79999999999");
         form.$("[data-test-id=agreement]").click();
         form.$("button.button").click();
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
-                .shouldBe(Condition.text("Встреча успешно забронирована на "));
-
+                .shouldBe(Condition.text("Встреча успешно забронирована на " + actualDate()));
 
 
     }
-
-
 
 
 }
